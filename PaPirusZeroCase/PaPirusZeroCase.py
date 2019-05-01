01c184a12282e4226b53f1fbc16de89b7707dff6
Tom Manner <tsmanner@us.ibm.com>
2019-05-01 10:44:08 -0400

    Started implementing classes for PCBs.

--- PaPirusZeroCase/PaPirusZeroCase.py
+++ PaPirusZeroCase/PaPirusZeroCase.py
@@ -1,5 +1,24 @@
+import os
 from solid import *
-from PiZeroW import pizerow
+from PiZeroW import *
 
 
-scad_render_to_file(pizerow, "PaPirusZeroCase.scad")
+bed = cube(
+    (
+        PIZW_LENGTH + 6,
+        PIZW_WIDTH + 6,
+        1
+    ),
+    center=True,
+)
+
+bed = translate((0, 0, -2))(bed)
+
+
+model = union()(
+    pizerow,
+    # bed,
+)
+
+
+scad_render_to_file(model, os.path.join(os.path.dirname(__file__), "PaPirusZeroCase.scad"))
--- PaPirusZeroCase/PiZeroW.py
+++ PaPirusZeroCase/PiZeroW.py
@@ -1,57 +1,134 @@
 from solid import *
 
+
+__all__ = [
+    "PCB_THICKNESS",
+    "PIZW_LENGTH",
+    "PIZW_WIDTH",
+    "pizerow",
+]
+
+
 PCB_THICKNESS = 1.6
+PIZW_LENGTH = 65
+PIZW_WIDTH = 30
 
 
-# Corner Radius 3mm
-# Mounting Holes # M2.5
-corner = polyhedron(
-    points = [
-        # Top Face
-        (-3, -3, PCB_THICKNESS/2),  # 0
-        (-3,  3, PCB_THICKNESS/2),  # 1
-        ( 0,  3, PCB_THICKNESS/2),  # 2
-        ( 3,  0, PCB_THICKNESS/2),  # 3
-        ( 3, -3, PCB_THICKNESS/2),  # 4
-        # Bottom Face
-        (-3, -3, -PCB_THICKNESS/2),  # 5
-        (-3,  3, -PCB_THICKNESS/2),  # 6
-        ( 0,  3, -PCB_THICKNESS/2),  # 7
-        ( 3,  0, -PCB_THICKNESS/2),  # 8
-        ( 3, -3, -PCB_THICKNESS/2),  # 9
-    ],
-    faces = [
-        (0, 1, 2, 3, 4),
-        (5, 6, 1, 0),
-        (6, 7, 2, 1),
-        (7, 8, 3, 2),
-        (8, 9, 4, 3),
-        (9, 5, 0, 4),
-        (9, 8, 7, 6, 5),
-    ],
-)
+class SolidObject:
+    def __init__(self, parent=None, coordinate=(0, 0, 0)):
+        self.parent = parent
+        self.coordinate = coordinate
+        self.children = []
 
-corner = union()(
-    cylinder(r=3, h=PCB_THICKNESS, segments=32, center=True),
-    corner,
-)
+    def addChild(self, child):
+        child.parent = self
+        self.children.append(child)
 
-corner = hull()(corner)
+    @property
+    def coordinate(self):
+        if self.parent:
+            return (
+                self.parent.coordinate[0] + self._coordinate[0],
+                self.parent.coordinate[1] + self._coordinate[1],
+                self.parent.coordinate[2] + self._coordinate[2],
+            )
+        return self._coordinate
 
-corner = difference()(
-    corner,
-    cylinder(r=1.25, h=PCB_THICKNESS, segments=32, center=True),
-)
+    @coordinate.setter
+    def coordinate(self, coordinate):
+        self._coordinate = coordinate
+
+
+class PCBMountingHole(SolidObject):
+    def __init__(self, radius, thickness, parent=None, coordinate=(0, 0, 0)):
+        super().__init__(parent, coordinate)
+        self.radius = radius
+        self.thickness = thickness
+
+    def geometry(self, parent_geometry=None):
+        this_geometry = cylinder(r=self.radius, h=self.thickness, segments=32, center=True)
+        if parent_geometry:
+            this_geometry = difference()(
+                parent_geometry,
+                this_geometry
+            )
+        this_geometry = translate(self.coordinate)(this_geometry)
+        return this_geometry
+
+
+class PCB(SolidObject):
+    def __init__(self, length, width, thickness, corner_radius, parent=None, coordinate=(0, 0, 0)):
+        super().__init__(parent, coordinate)
+        self.length = length
+        self.width = width
+        self.thickness = thickness
+        self.corner_radius = corner_radius
+        self.x_corner_displacement = self.length / 2 - self.corner_radius
+        self.y_corner_displacement =self.width / 2 - self.corner_radius
+
+    def _corner(self):
+        corner = polyhedron(
+            points = [
+                # Top Face
+                (-self.corner_radius, -self.corner_radius,  self.thickness/2),  # 0
+                (-self.corner_radius,  self.corner_radius,  self.thickness/2),  # 1
+                ( 0                 ,  self.corner_radius,  self.thickness/2),  # 2
+                ( self.corner_radius,  0                 ,  self.thickness/2),  # 3
+                ( self.corner_radius, -self.corner_radius,  self.thickness/2),  # 4
+                # Bottom Face
+                (-self.corner_radius, -self.corner_radius, -self.thickness/2),  # 5
+                (-self.corner_radius,  self.corner_radius, -self.thickness/2),  # 6
+                ( 0                 ,  self.corner_radius, -self.thickness/2),  # 7
+                ( self.corner_radius,  0                 , -self.thickness/2),  # 8
+                ( self.corner_radius, -self.corner_radius, -self.thickness/2),  # 9
+            ],
+            faces = [
+                (0, 1, 2, 3, 4),
+                (5, 6, 1, 0),
+                (6, 7, 2, 1),
+                (7, 8, 3, 2),
+                (8, 9, 4, 3),
+                (9, 5, 0, 4),
+                (9, 8, 7, 6, 5),
+            ],
+        )
+        corner = union()(
+            cylinder(r=self.corner_radius, h=self.thickness, segments=32, center=True),
+            corner,
+        )
+        corner = hull()(corner)
+        return corner
+
+    def geometry(self, parent_geometry=None):
+        this_geometry = union()(
+            cube((self.length - 4 * self.corner_radius, self.width, PCB_THICKNESS), center=True),
+            translate((-self.x_corner_displacement, 0, 0))(cube((2 * self.corner_radius, self.width - 4 * self.corner_radius, PCB_THICKNESS), center=True)),
+            translate(( self.x_corner_displacement, 0, 0))(cube((2 * self.corner_radius, self.width - 4 * self.corner_radius, PCB_THICKNESS), center=True)),
+            translate(( self.x_corner_displacement, self.y_corner_displacement, 0))(rotate((0, 0,   0))(self._corner())),
+            translate((-self.x_corner_displacement, self.y_corner_displacement, 0))(rotate((0, 0,  90))(self._corner())),
+            translate((-self.x_corner_displacement,-self.y_corner_displacement, 0))(rotate((0, 0, 180))(self._corner())),
+            translate(( self.x_corner_displacement,-self.y_corner_displacement, 0))(rotate((0, 0, 270))(self._corner())),
+        )
+        this_geometry = hull()(this_geometry)
+        if parent_geometry:
+            this_geometry = union()(
+                parent_geometry,
+                this_geometry
+            )
+        for child in self.children:
+            this_geometry = child.geometry(this_geometry)
+        this_geometry = translate(self.coordinate)(this_geometry)
+        return this_geometry
 
 
 # PCB Dimensions (mm)
 #   65 x 30 x 1.6
-pizerow = union()(
-    cube((53, 30, PCB_THICKNESS), center=True),
-    translate((-29.5, 0, 0))(cube((6, 18, PCB_THICKNESS), center=True)),
-    translate(( 29.5, 0, 0))(cube((6, 18, PCB_THICKNESS), center=True)),
-    translate(( 29.5,  12, 0))(rotate((0, 0,   0))(corner)),
-    translate((-29.5,  12, 0))(rotate((0, 0,  90))(corner)),
-    translate((-29.5, -12, 0))(rotate((0, 0, 180))(corner)),
-    translate(( 29.5, -12, 0))(rotate((0, 0, 270))(corner)),
-)
+# Corner Radius 3mm
+# Mounting Holes # M2.5
+pizw = PCB(65, 30, PCB_THICKNESS, 3)
+pizw.addChild(PCBMountingHole(1.25, PCB_THICKNESS, coordinate=( pizw.x_corner_displacement, pizw.y_corner_displacement, 0)))
+pizw.addChild(PCBMountingHole(1.25, PCB_THICKNESS, coordinate=(-pizw.x_corner_displacement, pizw.y_corner_displacement, 0)))
+pizw.addChild(PCBMountingHole(1.25, PCB_THICKNESS, coordinate=(-pizw.x_corner_displacement,-pizw.y_corner_displacement, 0)))
+pizw.addChild(PCBMountingHole(1.25, PCB_THICKNESS, coordinate=( pizw.x_corner_displacement,-pizw.y_corner_displacement, 0)))
+
+pizerow = pizw.geometry()
