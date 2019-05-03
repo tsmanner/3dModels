eb14f7e6df4a30e217e3cf2f2ff26bae583870d3
Tom Manner <tsmanner@us.ibm.com>
2019-05-03 16:52:32 -0400

    Fixed bug in RoundedCube.py where a generator object was being used for the coordinate instead of a tuple.

--- solidext/RoundedCube.py
+++ solidext/RoundedCube.py
@@ -3,10 +3,18 @@ from solid import cube, minkowski, sphere, translate
 
 def rounded_cube(size, corner_radius, segments=None, center=False):
     if isinstance(size, (int, float)):
-        size = (size - corner_radius,) * 3
+        size = (
+            size - 2 * corner_radius,
+            size - 2 * corner_radius,
+            size - 2 * corner_radius,
+        )
     else:
-        size = (item - corner_radius for item in size)
-    return translate((corner_radius,)*3)(
+        size = (
+            size[0] - 2 * corner_radius,
+            size[1] - 2 * corner_radius,
+            size[2] - 2 * corner_radius,
+        )
+    return translate((corner_radius, corner_radius, corner_radius))(
         minkowski()(
             cube(size),
             sphere(corner_radius, segments=segments)
