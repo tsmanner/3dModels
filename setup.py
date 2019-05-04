9784a831ebebd435046c943cde765140838d63fb
Tom Manner <zephrincochrane@gmail.com>
2019-05-04 09:42:41 -0400

    Added a setup.py for solidext package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="solidext",
+    install_requires=[
+        "solidpython",
+    ],
+    packages = [
+        "solidext",
+    ],
+)
